7bbeacf973fc0ef3679f72e224570f69b80b63d6
Christine Nguyen <ctine.ngyn@gmail.com>
2021-07-24 01:28:32 -0700

    Generate 50 female users from API

---
+++ src/utils/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// To use in another file
+export default {
+    getUser: function() {
+        // Generate 50 female users
+        return axios.get("https://randomuser.me/api/?gender=female&results=50")
+    }
+}
